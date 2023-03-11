5a310bf6a1a7f0bd78181ec28f1105419b6ba6f1
Administrator <109837883+cuidnuice@users.noreply.github.com>
2023-03-11 17:27:57 +0900

    458484

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,11 +6,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
             int count = 20;
-=======
-            int count = 30;
->>>>>>> 490dda48b2d5a0d590498e139fe0f0eb7f70cd1f
+
             
             Console.WriteLine("Hello World!");
         }
